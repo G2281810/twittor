a30954d8c7dae6fc1aefcebcba1f17a4b2598e4d
Gustavo Angel Santos Clemente <al221810743@gmail.com>
2019-12-05 03:56:45 -0600

    mejoramiento ios

--- sw.js
+++ sw.js
@@ -8,8 +8,8 @@ importScripts('js/sw-utils.js');
 
 
 
-const STATIC_CACHE   =  'static-v3';
-const DYNAMIC_CACHE  =  'dynamic-v1';
+const STATIC_CACHE   =  'static-v4';
+const DYNAMIC_CACHE  =  'dynamic-v2';
 const INMUTABLE_CACHE=  'inmutable-v1';
 
 //No se pueden modificar
@@ -53,9 +53,9 @@ self.addEventListener('activate', e =>{
 
     const respuesta = caches.keys().then(keys=>{
         keys.forEach(key=>{
-
           if (key!== STATIC_CACHE && key.includes('static') ) {
-
+          }
+          if (key!== DYNAMIC_CACHE && key.includes('dynamic') ) {
           }
         });
     });
