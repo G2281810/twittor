f3070af76a6ba739e93c320b5702a3242a1c411d
Gustavo Angel Santos Clemente <al221810743@gmail.com>
2019-12-05 01:03:53 -0600

    anexo del archivo utils e modo offline iconos

--- sw.js
+++ sw.js
@@ -1,4 +1,14 @@
-const STATIC_CACHE   =  'static-v1';
+
+//importaciones
+
+
+importScripts('js/sw-utils.js');
+
+
+
+
+
+const STATIC_CACHE   =  'static-v2';
 const DYNAMIC_CACHE  =  'dynamic-v1';
 const INMUTABLE_CACHE=  'inmutable-v1';
 
@@ -13,7 +23,8 @@ const APP_SHELL = [
   'img/avatars/spiderman.jpg',
   'img/avatars/thor.jpg',
   'img/avatars/wolverine.jpg',
-  'js/app.js'
+  'js/app.js',
+  'js/sw-utils.js'
 ];
 
 
@@ -33,7 +44,7 @@ self.addEventListener('install', e=> {
   const cacheInmutable =  caches.open(INMUTABLE_CACHE).then(cache=>
   cache.addAll( APP_SHELL_INMUTABLE ));
 
-  e.waitUntil(promise.all([ cacheStatic, cacheInmutable])  );
+  e.waitUntil(Promise.all([ cacheStatic, cacheInmutable])  );
 });
 
 
@@ -50,4 +61,18 @@ self.addEventListener('activate', e =>{
     });
 
     e.waitUntil( respuesta );
+});
+
+
+self.addEventListener( 'fetch', e =>{
+  const respuesta = caches.match( e.request ).then(res=>{
+    if (res) {
+      return res;
+    }else{
+        return fetch( e.request).then(newRes =>{
+            return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes);
+        });
+    }
+  });
+  e.respondWith( respuesta );
 });
